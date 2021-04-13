d109996e89d5956a39e91e1988436706a766ff1c
karanOO89 <gk.987680@gmail.com>
2021-04-13 03:55:02 +0000

    Testing with body parser done

--- express_server.js
+++ express_server.js
@@ -24,13 +24,17 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  console.log(req.params.shortURL);
-  const templateVars = {
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
-  };
-  res.render("urls_show", templateVars);
+    console.log(req.params.shortURL);
+    const templateVars = {
+        shortURL: req.params.shortURL,
+        longURL: urlDatabase[req.params.shortURL],
+    };
+    res.render("urls_show", templateVars);
 });
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
 
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
