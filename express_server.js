51a41f935be257efe2dc92c5659966f9ccc9c021
karanOO89 <gk.987680@gmail.com>
2021-04-13 02:52:35 +0000

    _header partial added

--- express_server.js
+++ express_server.js
@@ -17,7 +17,7 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
   });
 
-  app.get("/urls/:shortURL", (req, res) => {
+  app.get("/urls/:shortURL", (req, res) => {    
       console.log(req.params.shortURL)
     const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
     res.render("urls_show", templateVars);
