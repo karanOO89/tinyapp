606d42524f03517d489bf1938f6dc7104cf8a98d
karanOO89 <gk.987680@gmail.com>
2021-04-13 19:56:23 +0000

    shortUrl gets generated and assigned to long url with random string function

--- express_server.js
+++ express_server.js
@@ -1,9 +1,21 @@
+function generateRandomString() {
+  let result = "";
+  const length = 6;
+  const chars =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (let i = length; i > 0; --i)
+    result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
+
+// console.log(rString);
+
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = 8080;
 app.set("view engine", "ejs");
@@ -13,7 +25,7 @@ app.use(morganMiddleware);
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  "9sm5xK": "http://www.google.com"
 };
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -24,23 +36,21 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-    console.log(req.params.shortURL);
-    const templateVars = {
-        shortURL: req.params.shortURL,
-        longURL: urlDatabase[req.params.shortURL],
-    };
-    res.render("urls_show", templateVars);
+  console.log(req.params.shortURL);
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
 });
 app.post("/urls", (req, res) => {
-    console.log(req.body);  // Log the POST request body to the console
-    res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  });
-  function randomString(length, chars) {
-    let result = '';
-    for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-    return result;
-}
-let rString = randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  console.log(urlDatabase); // Log the POST request body to the console
+
+  res.redirect(`/urls/${shortURL}`); // Respond with 'Ok' (we will replace this)
+});
 
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
