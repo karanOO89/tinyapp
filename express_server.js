92a0dc9fa84edb31df40127aa8f8a068de30726c
karanOO89 <gk.987680@gmail.com>
2021-04-13 03:43:17 +0000

    New and body parser added

--- express_server.js
+++ express_server.js
@@ -1,31 +1,36 @@
 const express = require("express");
 const app = express();
-const morgan = require('morgan')
+const morgan = require("morgan");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const PORT = 8080;
 app.set("view engine", "ejs");
 
-const morganMiddleware = morgan('dev');
+const morganMiddleware = morgan("dev");
 app.use(morganMiddleware);
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
-  });
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
-  app.get("/urls/:shortURL", (req, res) => {    
-      console.log(req.params.shortURL)
-    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-    res.render("urls_show", templateVars);
-  });
-  app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
-  });
-
+app.get("/urls/:shortURL", (req, res) => {
+  console.log(req.params.shortURL);
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
+});
 
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
