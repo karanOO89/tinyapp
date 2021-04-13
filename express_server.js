bb7db0c36c2de0e6e4d86300c16d1bb454c54df7
karanOO89 <gk.987680@gmail.com>
2021-04-13 00:31:39 +0000

    Tested the webpage with curl command

--- express_server.js
+++ express_server.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const morgan = require('morgan')
+
+const PORT = 8080;
+
+const morganMiddleware = morgan('dev');
+app.use(morganMiddleware);
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
+  res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
