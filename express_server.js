21e04849bf49035ea7603f6599d40d35dea679cc
karanOO89 <gk.987680@gmail.com>
2021-04-13 03:15:22 +0000

    New url is added

--- express_server.js
+++ express_server.js
@@ -22,6 +22,9 @@ app.get("/urls", (req, res) => {
     const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
     res.render("urls_show", templateVars);
   });
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
 
 
 // app.get("/", (req, res) => {
