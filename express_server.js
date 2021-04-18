0395546c0abc3fa932d5770a90d239593b8b9fd8
karanOO89 <gk.987680@gmail.com>
2021-04-18 04:07:37 +0000

    code rectifications after rejection

--- express_server.js
+++ express_server.js
@@ -20,7 +20,7 @@ const morganMiddleware = morgan("dev");
 app.use(morganMiddleware);
 const bcrypt = require("bcrypt");
 
-function generateRandomString() {
+const generateRandomString = () => {
   let result = "";
   const length = 6;
   const chars =
@@ -28,8 +28,8 @@ function generateRandomString() {
   for (let i = length; i > 0; --i)
     result += chars[Math.floor(Math.random() * chars.length)];
   return result;
-}
-function generateRandomID() {
+};
+const generateRandomID = () => {
   let result = "";
   const length = 2;
   const chars =
@@ -37,17 +37,17 @@ function generateRandomID() {
   for (let i = length; i > 0; --i)
     result += chars[Math.floor(Math.random() * chars.length)];
   return result;
-}
+};
 
-function loginLookup(email, pass) {
+const loginLookup = (email, pass) => {
   const user = getUserByEmail(email, users);
   return user && bcrypt.compareSync(pass, user["password"]);
-}
-function findIdByEmail(email) {
+};
+const findIdByEmail = (email) => {
   const user = getUserByEmail(email, users);
   if (user) return user["id"];
-}
-function getShortUrlById(userId) {
+};
+const getShortUrlById = (userId) => {
   let keys = Object.keys(urlDatabase);
   let obj = {};
   for (let uid of keys) {
@@ -56,66 +56,71 @@ function getShortUrlById(userId) {
     }
   }
   return obj;
-}
-function getEmailByUserId(userId) {
+};
+const getEmailByUserId = (userId) => {
   for (let id in users) {
     if (users[id]["id"] === userId) {
       return users[id]["email"];
     }
   }
-}
+};
 
 const urlDatabase = {
   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
-  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48l" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" },
 };
 const users = {
   userRandomID: {
     id: "aJ48lW",
     email: "user@example.com",
-    password: "123",
+    password: bcrypt.hashSync("123", 10),
   },
 };
 
 app.get("/userReg", (req, res) => {
   const user = req.session["user_id"];
   if (!user) {
-    templateVars = { user: user };
-    res.render("user_registration", templateVars);
+    const templateVars = { user: user };
+    return res.render("user_registration", templateVars);
   } else {
-    res
+    return res
       .status(400)
       .send(
         "You are already loggedIn, please logout and click register for new registration"
       );
   }
 });
+
+
+
 app.post("/userReg", (req, res) => {
   let email = req.body.email;
-
   let Password = req.body.password;
 
   const password = bcrypt.hashSync(Password, 10);
 
   if (email === "" || password === "") {
-    res.status(400).send("Enter Valid Credentials"); //=> credential & email lookup in database
+    return res.status(400).send("Enter Valid Credentials"); //=> credential & email lookup in database
   }
-
-  if (getUserByEmail(email)) {
-    res.status(400).send("Email Already Exists");
+  if (getUserByEmail(email,users)) {
+    
+    return res.status(400).send("Email Already Exists");
   }
   let id = generateRandomID();
   users[id] = { id: id, email: email, password: password };
   req.session["user_id"] = id;
-  res.redirect("/urls");
+  return res.redirect("/urls");
 });
+
+
+
 app.get("/userLogin", (req, res) => {
   const user = req.session["user_id"];
   if (!user) {
-    templateVars = { user: "" };
-    res.render("user_login", templateVars);
+    const templateVars = { user: "" };
+    return res.render("user_login", templateVars);
   } else {
-    res.status(400).send("You are already loggedIn");
+    return res.status(400).send("You are already loggedIn");
   }
 });
 
@@ -123,37 +128,37 @@ app.post("/userLogin", (req, res) => {
   const email = req.body.email.trim();
   const password = req.body.password;
   if (email === "" || password === "") {
-    res.status(400).send("Enter Valid Credentials"); //=> credential & email lookup in database
+    return res.status(400).send("Enter Valid Credentials"); //=> credential & email lookup in database
   }
   if (!loginLookup(email, password)) {
-    res.status(400).send("Either Email Or Password Is Incorrect");
+    return res.status(400).send("Either Email Or Password Is Incorrect");
   }
   const id = findIdByEmail(email);
   req.session["user_id"] = id;
-  res.redirect("/urls");
+  return res.redirect("/urls");
+
 });
 
 //user registration
 
 app.get("/urls", (req, res) => {
-  const userId = req.session["user_id"];
+  const userId = req.session["user_id"];                   //=>main /url pages(GET)
   const user = getEmailByUserId(userId);
   const url = getShortUrlById(userId);
   if (userId) {
     const templateVars = { urls: url, user: user };
-
-    res.render("urls_index", templateVars);
+    return res.render("urls_index", templateVars);
+  } else {
+    return res.status(403).send("Login To Access This Page");
   }
-  res.status(403).send("Login To Access This Page");
 });
 app.post("/urls", (req, res) => {
-  //=>main url pages
-  const userID = req.session["user_id"];
+  const userID = req.session["user_id"];                    //=>main /url pages(POST)
   const longURL = req.body.longURL;
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = { longURL, userID };
 
-  res.redirect(`/urls`);
+  return res.redirect(`/urls`);
 });
 app.get("/urls/new", (req, res) => {
   const userId = req.session["user_id"];
@@ -161,9 +166,9 @@ app.get("/urls/new", (req, res) => {
 
   if (userId) {
     const templateVars = { user: user };
-    res.render("urls_new", templateVars);
+    return res.render("urls_new", templateVars);
   }
-  res.status(403).send("Login To Access This Page");
+  return res.status(403).send("Login To Access This Page");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -175,23 +180,23 @@ app.get("/urls/:shortURL", (req, res) => {
       longURL: urlDatabase[req.params.shortURL]["longURL"],
       user: user,
     };
-    res.render("urls_show", templateVars);
+    return res.render("urls_show", templateVars);
   }
-  res.status(403).send("Login To Access This Page");
+  return res.status(403).send("Login To Access This Page");
 });
 app.post("/urls/:shortURL/update", (req, res) => {
   //=> added update method
   const toUpdateUrl = req.body.longURL;
   urlDatabase[req.params.shortURL]["longURL"] = toUpdateUrl;
 
-  res.redirect("/urls");
+  return res.redirect("/urls");
 });
 
 app.get("/u/:shortURL", (req, res) => {
   //=> extracting the original long url from database
 
   const longURL = urlDatabase[req.params.shortURL]["longURL"];
-  res.redirect(longURL);
+  return res.redirect(longURL);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -199,13 +204,13 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
   const urlToDelete = req.params.shortURL;
   delete urlDatabase[urlToDelete];
-  res.redirect("/urls");
+  return res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
   req.session = null;
 
-  res.redirect("/");
+  return res.redirect("/");
 });
 
 app.get("/", (req, res) => {
@@ -216,8 +221,13 @@ app.get("/", (req, res) => {
   for (let ele in urlDatabase) {
     urls[ele] = urlDatabase[ele]["longURL"];
   }
-  const templateVars = { urls: urls, user: user };
-  res.render("home", templateVars);
+  if (userId) {
+    const templateVars = { urls: urls, user: user };
+    return res.render("urls_index", templateVars);
+  } else {
+    return res.redirect('/userLogin');
+  }
+ 
 });
 
 app.listen(PORT, () => {
--- helpers.js
+++ helpers.js
@@ -1,9 +1,11 @@
 const getUserByEmail =  (email, users)=> {
-    for (let key in users) {
-      if (users[key]["email"] === email) return users[key];
+  for (let key in users) {
+    if (users[key]["email"] === email) {
+    return users[key];
     }
-    return false; ;
-  };
+  }
+  return false; ;
+};
 
 
-  module.exports ={getUserByEmail};
+module.exports ={getUserByEmail};
